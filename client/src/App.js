e0204fc47772f178870a7f261234bbd2788fe3cf
anonewb <anonewbe@gmail.com>
2020-04-18 11:43:31 +0530

    #3 cleanup, navbar and router setup sone

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
+import Navbar from './components/layout/Navbar';
+import Home from './components/pages/Home';
+import About from './components/pages/About';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learning React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <>
+        <Navbar />
+        <div className='container'>
+          <Switch>
+            <Route path='/' exact>
+              <Home />
+            </Route>
+            <Route path='/about' exact>
+              <About />
+            </Route>
+          </Switch>
+        </div>
+      </>
+    </Router>
   );
 }
 
