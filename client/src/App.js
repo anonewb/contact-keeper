38518fb7f3b3d6bb1fb96b275cc39c1840d09e01
anonewb <anonewbe@gmail.com>
2020-04-21 19:29:41 +0530

    #4 Alert context, state and component

--- client/src/App.js
+++ client/src/App.js
@@ -5,31 +5,36 @@ import About from './components/pages/About';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import AuthState from './context/auth/AuthState';
 import ContactState from './context/contact/ContactState';
+import AlertState from './context/alert/AlertState';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
+import Alerts from './components/layout/Alerts';
 import './App.css';
 
 function App() {
   return (
     <AuthState>
       <ContactState>
-        <Router>
-          <>
-            <Navbar />
-            <div className='container'>
-              <Switch>
-                <Route path='/' exact>
-                  <Home />
-                </Route>
-                <Route path='/about' exact>
-                  <About />
-                </Route>
-                <Route exact path='/register' component={Register} />
-                <Route exact path='/login' component={Login} />
-              </Switch>
-            </div>
-          </>
-        </Router>
+        <AlertState>
+          <Router>
+            <>
+              <Navbar />
+              <div className='container'>
+                <Alerts />
+                <Switch>
+                  <Route path='/' exact>
+                    <Home />
+                  </Route>
+                  <Route path='/about' exact>
+                    <About />
+                  </Route>
+                  <Route exact path='/register' component={Register} />
+                  <Route exact path='/login' component={Login} />
+                </Switch>
+              </div>
+            </>
+          </Router>
+        </AlertState>
       </ContactState>
     </AuthState>
   );
