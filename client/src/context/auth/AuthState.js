f2f67065be9a7accec3e074942c424af164ebb0a
anonewb <anonewbe@gmail.com>
2020-04-23 18:39:02 +0530

    #4 User login

--- client/src/context/auth/AuthState.js
+++ client/src/context/auth/AuthState.js
@@ -68,8 +68,31 @@ const AuthState = (props) => {
     }
   };
 
-  // Login User	  // Login User
-  const login = () => console.log('login');
+  // Login User
+  const login = async (formData) => {
+    // console.log('login');
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+
+    try {
+      const res = await axios.post('/api/auth', formData, config);
+
+      dispatch({
+        type: LOGIN_SUCCESS,
+        payload: res.data,
+      });
+
+      loadUser();
+    } catch (err) {
+      dispatch({
+        type: LOGIN_FAIL,
+        payload: err.response.data.msg,
+      });
+    }
+  };
 
   // Logout	  // Logout
   const logout = () => console.log('logout');
